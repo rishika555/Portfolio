7f0b5c17f123c646aa4fee45a2e8984461bd467f
Rishika Jain <148350709+rishika555@users.noreply.github.com>
2025-01-14 14:22:41 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,14 @@ export default function Navbar() {
   return (
     <header className="bg-gradient shadow-lg md:sticky top-0 z-10">
       {/* Flex container */}
-      <div className="container mx-auto flex items-center justify-between">        
+      <div className="container mx-auto flex items-center justify-between px-4">
+        {/* Title */}
+        <div className="title-font">
+          My Website
+        </div>
+
         {/* Right-aligned navigation links */}
-        <nav>
+        <nav className="flex gap-4">
           <Link 
             to="about" 
             smooth={true} 
